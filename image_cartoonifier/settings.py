a280a688133a8c9d1c04d3482d970451f461996d
jayantkhanna1 <jayantkhanna3105@gmail.com>
2022-12-07 00:17:19 +0530

    first commit

--- image_cartoonifier/settings.py
+++ image_cartoonifier/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'image_cartoonifier_app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
