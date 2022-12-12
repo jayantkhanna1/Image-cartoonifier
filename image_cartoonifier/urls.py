76e16ee2ce178654265f38a66e7592972d081716
jayantkhanna1 <jayantkhanna3105@gmail.com>
2022-12-13 00:21:55 +0530

    first commit

--- image_cartoonifier/urls.py
+++ image_cartoonifier/urls.py
@@ -18,5 +18,5 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("",include("cartoonify.urls"))
+    path("",include("image_cartoonifier_app.urls"))
 ]
